ec7f30973e0a693e5a2f56fa8931f3d8f1cb0268
Unabandonware <126729444+Unabandonware@users.noreply.github.com>
2025-11-26 00:32:47 -0500

    Add interleave scripts by ZiplineGun

---
+++ BeforeExtraction/interleave_2.py
@@ -0,0 +1,17 @@
+import sys
+import re
+
+CHUNK_SIZE = 2
+
+with open(sys.argv[1], "rb") as inf, open(sys.argv[2], "rb") as inf2, \
+     open(sys.argv[1]+"_2interleaved.bin", "wb") as outf:
+    while True:
+        chunk1 = inf.read(CHUNK_SIZE)
+        chunk2 = inf2.read(CHUNK_SIZE)
+        
+        if not chunk1 and not chunk2: break
+        
+        if chunk1:
+            outf.write(chunk1)
+        if chunk2:
+            outf.write(chunk2)
